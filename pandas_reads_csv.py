b3ea87d0d1f62437887f6daf563562c5b2d9b574
alexHooper <alexmohack@gmail.com>
2018-09-06 10:34:03 +0530

    write previous code for learning

--- pandas_reads_csv.py
+++ pandas_reads_csv.py
@@ -1,4 +1,17 @@
 import pandas
 
 df = pandas.read_csv('hrdata.csv')
-print(df)
+
+df_2 = pandas.read_csv('hrdata.csv', index_col='Name')
+
+df_3 = pandas.read_csv('hrdata.csv', index_col='Name', parse_dates=['Hire Date'])
+
+df_4 = pandas.read_csv(
+	'hrdata.csv',
+	index_col='Employee',
+	parse_dates=['Hired'],
+	header=0,
+	names=['Employee', 'Hired', 'Salary', 'Sick Days']
+)
+
+print(df_4)
