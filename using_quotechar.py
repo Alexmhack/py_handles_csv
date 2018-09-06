20cefd64c70f9e7007aed29612972c00ef2955ec
alexHooper <alexmohack@gmail.com>
2018-09-06 08:46:32 +0530

    print the column name and row values

--- using_quotechar.py
+++ using_quotechar.py
@@ -5,10 +5,9 @@ with open('employee_data1.csv') as csvfile:
 	line_count = 0
 	for row in csv_reader:
 		if line_count == 0:
-			print(f"Columns are {", ".join(row)}")
+			print(f"Columns are {', '.join(row)}")
 			line_count += 1
 		else:
-			print(f"\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.")
+			print(f"\t{row[0]} works in the {row[1]} department, and was born in {row[2]} and lives in {row[3]}.")
 			line_count += 1
 	print(f"Processed {line_count} lines")
-
