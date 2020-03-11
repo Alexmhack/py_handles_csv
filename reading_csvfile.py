dec03d12c41edd2ef7af8b35c1ec922c9e4a1494
Sowmith Mandadi <msowmith25@gmail.com>
2020-03-11 09:47:17 +0000

    reading_csvfile.py Changed read csv file

--- reading_csvfile.py
+++ reading_csvfile.py
@@ -8,6 +8,6 @@ with open('employee_data.csv') as csvfile:
 			print(f"Column names are {', '.join(row)}")
 			line_count += 1
 		else:
-			print(f"\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.")
+			print(f"\t{row[0]}  in the {row[1]} department, and was born in {row[2]}.")
 			line_count += 1
 	print(f"Processed {line_count} lines")
